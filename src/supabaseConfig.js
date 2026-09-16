fe25d3bdc4cc1fa24723bc850454b0a0af929320
rafga792 <juliantrapga@gmail.com>
2026-09-16 10:05:17 +0700

    Update supabaseConfig.js

--- src/supabaseConfig.js
+++ src/supabaseConfig.js
@@ -4,7 +4,7 @@ const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBh
 const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
     realtime: {
         params: {
-            eventsPerSecond: 30, // Mengoptimalkan jumlah paket data per detik agar jaringan HP tidak runtuh
+            eventsPerSecond: 30,
         },
     },
 });
