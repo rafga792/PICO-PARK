2fd0786cfeee919d273ffbae57ed0cba9d71a709
rafga792 <juliantrapga@gmail.com>
2026-09-16 10:01:50 +0700

    Update supabaseConfig.js

--- src/supabaseConfig.js
+++ src/supabaseConfig.js
@@ -1,7 +1,10 @@
-// Load Supabase SDK via CDN (Tambahkan di HTML)
-// <script src="https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2"></script>
-
 const SUPABASE_URL = 'https://jfzdummmhwnrzssdzdoa.supabase.co';
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpmemR1bW1taHducnpzc2R6ZG9hIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODk1MTQ5NTIsImV4cCI6MjEwNTA5MDk1Mn0.gua_DG8DwXUjBDSIfBZRFk8lSa8BpsAOeFPCggAQKmY';
 
-const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
+    realtime: {
+        params: {
+            eventsPerSecond: 30, // Mengoptimalkan jumlah paket data per detik agar jaringan HP tidak runtuh
+        },
+    },
+});
